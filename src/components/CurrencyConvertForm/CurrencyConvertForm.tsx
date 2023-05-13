d6b8f7cb51948e91c5bb39cf7ac1c411d8027058
macszl <235989@edu.p.lodz.pl>
2023-05-13 13:54:38 +0200

    feat: currency converter is working now

--- src/components/CurrencyConvertForm/CurrencyConvertForm.tsx
+++ src/components/CurrencyConvertForm/CurrencyConvertForm.tsx
@@ -1,107 +1,119 @@
-import { Button, InputLabel, TextField } from '@mui/material';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { Button, Grid, InputLabel, MenuItem, TextField, Typography } from '@mui/material';
+import Select from '@mui/material/Select';
 import axios from 'axios';
-import { Field, Formik } from 'formik';
+import { useFormik } from 'formik';
 import { useState } from 'react';
+import { CurrencyConverterFormErrors } from './CurrencyConverterForm.types';
+
+//todo
+//remove this after backend sync gets done
+
+const availableCurrencies = ['USD', 'JPY', 'GBP', 'EUR', 'INR', 'AUD'];
 
 export function CurrencyConvertForm() {
-  const [fromCurrency, setFromCurrency] = useState<string>('USD');
-  const [toCurrency, setToCurrency] = useState<string>('GBP');
-  const [conversionSuccessful, setConversionSuccessful] = useState(false);
-  const [amount, setAmount] = useState(0);
-  const [exchangeRate, setExchangeRate] = useState(0);
+  const [conversionResult, setConversionResult] = useState('');
 
-  const handleFromCurrencySelectChange = (event: SelectChangeEvent) => {
-    setFromCurrency(event.target.value as string);
-  };
-
-  const handleToCurrencySelectChange = (event: SelectChangeEvent) => {
-    setToCurrency(event.target.value as string);
-  };
-
-  const getExchangeRate = async () => {
-    const response = await axios.get(`${import.meta.env.VITE_API_URL}/exchangerates/rates/c/${fromCurrency}`);
+  const getExchangeRate = async (values: { amount: string; fromCurrency: string; toCurrency: string }) => {
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/exchangerates/rates/c/${values.fromCurrency}`);
     const fromRate = response.data.rates[0].bid;
 
-    const response2 = await axios.get(`${import.meta.env.VITE_API_URL}exchangerates/rates/c/${toCurrency}`);
+    const response2 = await axios.get(`${import.meta.env.VITE_API_URL}/exchangerates/rates/c/${values.toCurrency}`);
     const toRate = response2.data.rates[0].ask;
 
-    const rate = toRate / fromRate;
+    const rate = fromRate / toRate;
 
-    setExchangeRate(rate);
-    setConversionSuccessful(true);
+    setConversionResult(`${values.amount} ${values.fromCurrency} = ${Number(values.amount) * rate} ${values.toCurrency}`);
   };
 
-  const validate = (values: { inputCurrency: number; outputCurrency: number }) => {
-    return;
+  const validate = (values: { amount: string }) => {
+    const errors: CurrencyConverterFormErrors = {};
+
+    if (!values.amount || isNaN(parseFloat(values.amount))) {
+      errors.amount = 'Please enter a valid number';
+    }
+
+    return errors;
   };
+
+  const formik = useFormik({
+    initialValues: {
+      amount: '0',
+      fromCurrency: availableCurrencies[0],
+      toCurrency: availableCurrencies[1],
+    },
+    validate: validate,
+    onSubmit: (values) => {
+      getExchangeRate(values);
+    },
+  });
 
   return (
-    <>
-      <Formik
-        enableReinitialize
-        validateOnMount={true}
-        validateOnChange={true}
-        validateOnBlur={true}
-        validate={validate}
-        initialValues={{ inputCurrency: 0, outputCurrency: 0 }}
-        onSubmit={(values) => {
-          getExchangeRate();
-        }}
-      >
-        {({ isValid, handleSubmit }) => {
-          return (
-            <form
-              onSubmit={(event) => {
-                handleSubmit(event);
-              }}
-            >
-              <Field
-                as={TextField}
-                label='Amount'
-                sx={{ width: 1, backgroundColor: 'white' }}
-                name='Amount'
-                onChange={setAmount}
-                required
-              />
-              <InputLabel id='from-currency-label'>From:</InputLabel>
-              <Select
-                labelId='from-currency-label'
-                id='from-currency-select'
-                label='From'
-                value={fromCurrency}
-                onChange={handleFromCurrencySelectChange}
-              />
-              <InputLabel id='to-currency-label'>To:</InputLabel>
-              <Select
-                labelId='to-currency-label'
-                id='to-currency-select'
-                label='To'
-                value={toCurrency}
-                onChange={handleToCurrencySelectChange}
-              />
-
-              <Button
-                variant='contained'
-                color='primary'
-                sx={{
-                  padding: 1,
-                  width: 1,
-                }}
-                type='submit'
-                disabled={!isValid}
+    <Grid>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          id='amount'
+          name='amount'
+          label='Amount'
+          type='number'
+          value={formik.values.amount}
+          onChange={formik.handleChange}
+          error={formik.touched.amount && Boolean(formik.errors.amount)}
+        />
+        <InputLabel id='from-currency-label'>From:</InputLabel>
+        <Select
+          id='from-currency-select'
+          name='fromCurrency'
+          label='From'
+          value={formik.values.fromCurrency || ''}
+          onChange={formik.handleChange}
+          error={formik.touched.fromCurrency && Boolean(formik.errors.fromCurrency)}
+        >
+          {availableCurrencies.map((item, index) => {
+            return (
+              <MenuItem
+                value={item}
+                key={index}
               >
-                Convert
-              </Button>
-            </form>
-          );
-        }}
-      </Formik>
-      {conversionSuccessful && (
-        <div>
-          {amount} {fromCurrency} = {amount * exchangeRate} {toCurrency}
-        </div>
-      )}
-    </>
+                {item}
+              </MenuItem>
+            );
+          })}
+        </Select>
+        <InputLabel id='to-currency-label'>To:</InputLabel>
+        <Select
+          id='to-currency-select'
+          name='toCurrency'
+          label='To'
+          value={formik.values.toCurrency}
+          onChange={formik.handleChange}
+          error={formik.touched.toCurrency && Boolean(formik.errors.toCurrency)}
+        >
+          {availableCurrencies.map((item, index) => {
+            return (
+              <MenuItem
+                value={item}
+                key={index}
+              >
+                {item}
+              </MenuItem>
+            );
+          })}
+        </Select>
+
+        <Button
+          variant='contained'
+          color='primary'
+          sx={{
+            padding: 1,
+            width: 1,
+          }}
+          type='submit'
+          disabled={formik.errors.amount ? true : false}
+        >
+          Convert
+        </Button>
+        {conversionResult && <Typography> {conversionResult}</Typography>}
+      </form>
+    </Grid>
   );
 }
